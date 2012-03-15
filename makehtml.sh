78aee0ed2c293c1bbced926fc47b1b9dd716d350
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-03-15 22:54:18 +0300

    first commit

---
+++ makehtml.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ -f Zadeh-1965.en.idx ]; then
+  tex '\def\filename{{Zadeh-1965.en}{idx}{4dx}{ind}} \input  idxmake.4ht' 
+  makeindex -o Zadeh-1965.en.ind Zadeh-1965.en.4dx 
+fi
+
+htlatex Zadeh-1965.en.tex
